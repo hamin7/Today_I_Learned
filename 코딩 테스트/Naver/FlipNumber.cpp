4a326296d173beba350906b8e0a9ed72920f396e
hamin7 <33690039+hamin7@users.noreply.github.com>
2020-02-05 01:15:06 +0900

    Create FlipNumber.cpp

---
+++ 코딩 테스트/Naver/FlipNumber.cpp
@@ -0,0 +1,26 @@
+// you can use includes, for example:
+#include <algorithm>
+#include <vector>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int getFlipNumber(vector<int> &coins, int flag){
+    
+    int count = 0;
+    
+    for(int i=0; i<coins.size();i++){
+        if(coins[i]!=flag)
+            count++;
+        
+        flag = (flag ==0) ? 1: 0;
+    }
+    return count;
+    
+}
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    
+    
+    return min(getFlipNumber(A, 0), getFlipNumber(A,1));
+}
