81b3b246bf8c4e40c98b1495352d0acce0c64258
hamin7 <33690039+hamin7@users.noreply.github.com>
2020-02-05 01:04:39 +0900

    Update Sample.cpp

--- 코딩 테스트/Naver/Sample.cpp
+++ 코딩 테스트/Naver/Sample.cpp
@@ -9,26 +9,19 @@ int solution(vector<int> &A) {
 
     // write your code in C++14 (g++ 6.2.0)
     sort(A.begin(), A.end());
-    // unique(A.begin(), A.end());
+    unique(A.begin(), A.end());
     
     int index = 1;
-    for(int i=0;i<(int)A.size();){
+    for(int i=0;i<(int)A.size();i++){
         if(A[i]<=0){
-            i++;
             continue;
         }
             
         if(A[i]!=index)
             return index;
             
-        while(A[i]==index){
-            i++;
-        }
         index++;
-        
     }
     if(index==1) return 1;
     else return index;
-    
-    
 }
