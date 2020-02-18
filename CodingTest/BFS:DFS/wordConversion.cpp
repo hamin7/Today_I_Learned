23537b0044877d943dac9141aabc4e5431d00c75
hamin7 <33690039+hamin7@users.noreply.github.com>
2020-02-18 21:27:49 +0900

    Update wordConversion.cpp

--- CodingTest/BFS:DFS/wordConversion.cpp
+++ CodingTest/BFS:DFS/wordConversion.cpp
@@ -1,37 +1,46 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-bool visited[50];
+vector<bool> check; // 탐색한 단어 재탐색하지 않도록 처리.
 int answer = 51;
 
-void dfs(string begin, string target, vector<string> words, int depth){
-    int cmp_cnt = 0;
+void dfs(vector<string> words, string now, string target, int depth) {
+    // 완성 비교.
+    if (now.compare(target) == 0){
+        answer = min(answer, depth);  // answer와 depth 중 작은 값을 answer에 넣기.
+        return;
+    }
     
-    for (int i = 0; i < words.size(); i++){
-        if (visited[i])
+    // 한 개 차이나면 들어가기.
+    for (int i = 0; i < words.size(); i++) {
+        // now와 words의 단어가 하나만 다르면 변경 가능.
+        int diff = 0;
+        if (check[i] == true)   // 탐색했던 단어라면 재탐색 하지 말기.
             continue;
-        for (int j = 0; j < words[i].size(); j++){
-            if (begin[j] != words[i][j])   
-                cmp_cnt++;
-        }
         
-        if (cmp_cnt == 1){
-            if (words[i] == target && depth < answer) {
-                answer = depth;
-                return;
-            }
-            visited[i] = true;
-            dfs(words[i], target, words, depth + 1);
-            visited[i] = false;
+        // 스펠링 몇개가 다른지 판별.
+        for (int j = 0; (j < now.size())&&(diff <= 1); j++) {
+            if (now[j] != words[i][j])
+                diff++;
+        }
+             
+        // 스펠링 하나만 다르다면 변경.
+        if (diff == 1) {
+            check[i] = true;    // 탐색했음 표시.
+            dfs(words, words[i], target, depth+1);
+            check[i] = false;   // 탐색 끝.
         }
     }
 }
 
 int solution(string begin, string target, vector<string> words) {
-    dfs(begin, target, words, 1);
-    if (answer == 51)
+    // 변환할 수 없는 경우.
+    if (find(words.begin(), words.end(), target) == words.end())
         return 0;
+    check.assign(words.size(), false);  // 벡터 check를 모두 false로 초기화.
+    dfs(words, begin, target, 0);
     return answer;
 }
