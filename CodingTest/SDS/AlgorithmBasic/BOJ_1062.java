491cdab33c8ed2a3c776929fe67c0bc9360c194a
hamin <ggamini7@gmail.com>
2020-08-25 16:33:46 +0900

    edit about pro test problem

---
+++ CodingTest/SDS/AlgorithmBasic/BOJ_1062.java
@@ -0,0 +1,17 @@
+// package DAY01.P1062;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BOJ_1062 {
+    
+    static int N, K;
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src\\DAY01\\P1062\\input.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+}
