367b3607bc3e194ac6eab790868562b389b104ee
hamin <ggamini7@gmail.com>
2020-08-26 22:29:41 +0900

    upload

--- CodingTest/SDS/AlgorithmBasic/BOJ_1062.java
+++ CodingTest/SDS/AlgorithmBasic/BOJ_1062.java
@@ -11,16 +11,39 @@ antic 다섯개의 단어는 무조건 가르쳐야
 */
 // package DAY01.P1062;
 
+package Day01.P1062;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BOJ_1062 {
-    
-    static int N, K;
+	
+	static int N, K;
+	static String[] words;
+	static boolean[] visited;		// 방문 체크.
 
-    public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("src\\DAY01\\P1062\\input.txt"));
-        Scanner sc = new Scanner(System.in);
-    }
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("Hi");
+		
+		System.setIn(new FileInputStream("/Users/hamin/eclipse-workspace/SamsungSDS/src/Day01/P1062/input.txt"));     // 어떤 input을 기본 input으로 할 것인지.
+		Scanner sc = new Scanner(System.in);
+		
+		N = Integer.parseInt(sc.next());	// string으로 들어오는 input을 string으로.
+		K = Integer.parseInt(sc.next());
+		
+		System.out.println(N + ", " + K);
+		
+		words = new String[N];		// N개의 공간을 만들어줌.
+		visited = new boolean[26];		// 26개의 알파벳 visited 체크.
+		
+		for (int i = 0; i < N; i++) {
+			words[i] = sc.next().replaceAll("[antic]", "");		// 중복되는 글자들을 공백으로 바꾸기(공통되는 것 제거).
+		}
+		
+		System.out.println(Arrays.toString(words));
+		
+	}
+
 }
