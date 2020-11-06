6d35a9eba2d62aaac573efe1ffc36978203f7799
hamin <ggamini7@gmail.com>
2020-11-07 00:27:50 +0900

    upload

--- CodingTest/Programmers/DFS&&BFS/travelPath.java
+++ CodingTest/Programmers/DFS&&BFS/travelPath.java
@@ -3,47 +3,78 @@ import java.util.Collections;
 import java.util.List;
 
 class Solution {
-    static String route = "";
-    static boolean[] visit;
-    List<String> list = new ArrayList<>();
+    static boolean[] visited;
+    static String aviation = "";
+    ArrayList<String> candidateRoute = new ArrayList<>();
+    static String[][] tickets;
     
-    public String[] solution(String[][] tickets) {
-        for (int i = 0; i < tickets.length; i++) {
-            visit = new boolean[tickets.length];
-            String start = tickets[i][0];
-            String end = tickets[i][1];
-            
-            if(start.equals("ICN")) {
-                route = start + ",";
-                visit[i] = true;
-                dfs(tickets, end, 1);
+    public String[] solution(String[][] airTickets) {
+        String[] answer = {};
+        
+        tickets = new String[airTickets.length][airTickets[0].length];
+        for (int i = 0; i < airTickets.length; i++) {
+            for (int j = 0; j < airTickets[0].length; j++) {
+                tickets[i][j] = airTickets[i][j];
             }
         }
         
-        Collections.sort(list);
-        String[] answer = list.get(0).split(",");
+        for (int i = 0; i < tickets.length; i++) {
+            visited = new boolean[tickets.length];
+            
+            String departure = tickets[i][0];
+            String arrival = tickets[i][1];
+            
+            if (departure.equals("ICN")) {
+                boolean flag = false;
+                aviation = departure + ",";
+                visited[i] = true;
+                flag = dfs(arrival, 1);
+                
+            }
+        }
+        
+        Collections.sort(candidateRoute);
+//         String을 배열로 바꿔주기
+        answer = candidateRoute.get(0).split(",");
         
         return answer;
     }
     
-    public void dfs(String[][] tickets, String end, int cnt) {
-        route += end + ",";
+    public boolean dfs(String arrival, int aviationCount) {
+        aviation += arrival + ",";
         
-        if (cnt == tickets.length) {
-            list.add(route);
-            return;
+//         2. 목적지인가
+//         티켓을 다 사용 했다면, 끝
+        if (aviationCount == tickets.length) {
+            aviation = aviation.substring(0, aviation.length() - 1);
+            candidateRoute.add(aviation);
+            return true;
         }
         
+//         3. 갈 수 있는 곳을 순회
+//         다른 티켓 탐색
+//         다른 티켓 시작 == arriaval && visited == false
         for (int i = 0; i < tickets.length; i++) {
-            String s = tickets[i][0];
-            String e = tickets[i][1];
+            String start = tickets[i][0];
+            String end = tickets[i][1];
             
-            if (s.equals(end) && !visit[i]) {
-                visit[i] = true;
-                dfs(tickets, e, cnt + 1);
-                visit[i] = false;
-                route = route.substring(0, route.length() - 4);
+//             4. 갈 수 있는가
+//             전 단계 arrival과 이번 단계 start가 같고, 방문하지 않았다면
+            if (start.equals(arrival) && visited[i] == false) {
+                boolean flag = false;
+//                 1. 체크인
+                visited[i] = true;
+//                 5. 간다 dfs(next)
+                if (dfs(end, aviationCount + 1) == false) {
+//                 6. 체크아웃 -> 안해도 됨.
+                visited[i] = false;
+                // 메모리 사용을 줄이기 위해 return 후 aviation을 지워줄 것.
+                aviation = aviation.substring(0, aviation.length() - 4);
+                }
+
             }
         }
+        
+        return false;
     }
 }
